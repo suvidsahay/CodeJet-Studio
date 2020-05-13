5ffbd4addf21d2ae80938fc71df25e4b082aab63
Suvid Sahay <sahay.suvid61@gmail.com>
2020-05-13 06:39:05 +0530

    Save File done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+
+
 public class App {
     public static void main (String[] args) {
         LoginFrame l= new LoginFrame();
--- src/main/java/editor/Editor.java
+++ src/main/java/editor/Editor.java
@@ -1,29 +1,32 @@
 package editor;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.border.Border;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.*;
 import java.awt.Container;
 import java.io.*;
+import java.time.Clock;
 
 public class Editor extends JFrame implements ActionListener {
 
-    private JTextArea textArea = new JTextArea();
-    private ProjectStructure project = new ProjectStructure();
+    public static JTextArea textArea = new JTextArea();
+    private JPanel browser = new JPanel();
+    private JTree directory;
+    private JScrollPane scrollPaneBro = new JScrollPane();
+    private JScrollPane scrollPaneText = new JScrollPane();
+    File currentFile;
+    File currentDirectory;
 
-    public Editor() {        
-
+    public Editor() {
         setLayout(new BorderLayout());
-        Container c = getContentPane();
-
-
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        c.add(scrollPane,BorderLayout.CENTER);
-        c.add(project,BorderLayout.WEST);
 
         JMenuBar menuBar= new JMenuBar();
         setJMenuBar(menuBar);
@@ -31,52 +34,68 @@ public class Editor extends JFrame implements ActionListener {
         JMenu file = new JMenu("File");
         menuBar.add(file);
 
-        JMenuItem newFile = new JMenuItem("New");
-        JMenuItem openFile = new JMenuItem("Open File");
         JMenuItem openDirectory = new JMenuItem("Open Directory");
         JMenuItem saveFile = new JMenuItem("Save");
 
-        newFile.addActionListener(this);
-        openFile.addActionListener(this);
         openDirectory.addActionListener(this);
         saveFile.addActionListener(this);
 
-        file.add(newFile);
-        file.add(openFile);
         file.add(openDirectory);
         file.add(saveFile);
-        scrollPane.setViewportView(textArea);
 
-        setJMenuBar(menuBar);
+        add(menuBar, BorderLayout.NORTH);
+
+
+        scrollPaneBro.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPaneBro.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPaneText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPaneText.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        add(scrollPaneBro, BorderLayout.WEST);
+        add(scrollPaneText, BorderLayout.CENTER);
+        scrollPaneBro.setViewportView(browser);
+        scrollPaneText.setViewportView(textArea);
 
         setVisible(true);
         setSize(1920,1080);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    void openFile(File file) {
+        try {
+            String fileDataLine = "", fileData = "";
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((fileDataLine = br.readLine()) != null) {
+                fileData = fileData + "\n" + fileDataLine;
+            }
+            textArea.setText(fileData);
+
+        } catch (Exception evt) {
+            JOptionPane.showMessageDialog(this, evt.getMessage());
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         String s = actionEvent.getActionCommand();
-        if( s.equals("Open File") ) {
-            JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            int dialog = fileChooser.showOpenDialog(null);
-            if (dialog == JFileChooser.APPROVE_OPTION) {
-                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                try {
-                    String fileDataLine = "", fileData = "";
-                    FileReader fr = new FileReader(file);
-                    BufferedReader br = new BufferedReader(fr);
-
-                    while ((fileDataLine = br.readLine()) != null) {
-                        fileData = fileData + "\n" + fileDataLine;
-                    }
-                    textArea.setText(fileData);
-                } catch (Exception evt) {
-                    JOptionPane.showMessageDialog(this, evt.getMessage());
+        if( s.equals("Save") ) {
+            String textData = "";
+            textData = textArea.getText();
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(currentFile));
+                BufferedReader reader = new BufferedReader(new StringReader(textData));
+                writer.write("");
+                String[] lines = textData.split(System.getProperty("line.separator"));
+                for(int i = 0; i < lines.length; i++) {
+                    writer.append(lines[i]);
+                    writer.append("\n");
+                    System.out.println(lines[i]);
                 }
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            else
-                JOptionPane.showMessageDialog(this, "the user cancelled the operation");
         }
         else if (s.equals( "Open Directory" )) {
             JFileChooser directoryChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
@@ -84,12 +103,51 @@ public class Editor extends JFrame implements ActionListener {
             int dialog = directoryChooser.showOpenDialog(null);
             if( dialog == JFileChooser.APPROVE_OPTION) {
                 File file = directoryChooser.getSelectedFile();
-                project.displayFilesInDirectory(file);
-                revalidate();
-                repaint();
+                displayFilesInDirectory(file);
             }
             else
                 JOptionPane.showMessageDialog(this, "the user cancelled the operation");
+        }
+    }
+
+    void displayFilesInDirectory(File file){
+
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(file.getName());
+        generateTreeView(file, root);
+
+        directory = new JTree(root);
+
+        directory.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+        directory.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+        directory.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent treeSelectionEvent) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) treeSelectionEvent.getPath().getLastPathComponent();
+                openFile((File)node.getUserObject());
+                currentFile = (File)node.getUserObject();
+                currentDirectory = (File)((File) node.getUserObject()).getParentFile();
+            }
+        });
+        
+        directory.setVisible(true);
+        browser.add(directory);
+        scrollPaneBro.setViewportView(browser);
+        revalidate();
+        repaint();
+    }
+
+    private void generateTreeView(File rootFile, DefaultMutableTreeNode parent) {
+        File[] files = rootFile.listFiles();
+
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                DefaultMutableTreeNode node = new DefaultMutableTreeNode(files[i]);
+                parent.add(node);
+                if (files[i].isDirectory()) {
+                    generateTreeView(files[i], node);
+                }
+            }
         }
     }
 }
